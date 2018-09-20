ab789623e83929c224fb50a646388c174fc5ccb2
Krendelev <krendelev@gmail.com>
2018-09-21 00:48:56 +0300

    get issues from actual repo

--- github_trending.py
+++ github_trending.py
@@ -2,8 +2,8 @@ import datetime
 import requests
 
 
-def get_date(period):
-    return datetime.date.today() - datetime.timedelta(days=period)
+def get_starting_date(days):
+    return datetime.date.today() - datetime.timedelta(days=days)
 
 
 def get_trending_repositories(top_size, date):
@@ -16,12 +16,17 @@ def get_trending_repositories(top_size, date):
     return requests.get(url, params=payload).json()['items']
 
 
+def get_open_issues_amount(full_repo_name):
+    url = 'https://api.github.com/repos/{0}/issues'.format(full_repo_name)
+    return len(requests.get(url).json())
+
+
 def print_trending_repositories(repos):
     print('Stars Issues URL')
     for repo in top_repos:
-        print('{0:>5} {1:^6} {2}'.format(
+        print('{1:>5} {0:^6} {2}'.format(
+            get_open_issues_amount(repo['full_name']),
             repo['stargazers_count'],
-            repo['open_issues_count'],
             repo['html_url']
             )
         )
@@ -29,9 +34,11 @@ def print_trending_repositories(repos):
 
 if __name__ == '__main__':
     top_size = 20
-    period = 7
+    days = 7
     try:
-        top_repos = get_trending_repositories(top_size, get_date(period))
+        top_repos = get_trending_repositories(
+            top_size, get_starting_date(days)
+            )
     except requests.exceptions.ConnectionError:
         print('Failed to establish connection to api.github.com')
     else:
