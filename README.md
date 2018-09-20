a5384e2a22bd9ae9ec14b7a1382848698c0e82e0
Krendelev <krendelev@gmail.com>
2018-09-20 16:53:33 +0300

    implement main feature

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Github Trends
 
-[TODO. There will be project description]
+Let's check 20 most popular repositories on last week for open issues.
 
-# Project Goals
+## How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+## Quick Start
+
+```bash
+$ python github_trending.py
+Stars Issues URL
+  584   0    https://github.com/semlinker/awesome-typescript
+  467   1    https://github.com/TarsCloud/TarsGo
+  391   2    https://github.com/GoogleChromeLabs/worker-plugin
+  306   0    https://github.com/yboetz/motd
+  298   4    https://github.com/trailofbits/twa
+  ...
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,38 @@
+import datetime
+import requests
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_date(period):
+    return datetime.date.today() - datetime.timedelta(days=period)
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(top_size, date):
+    url = 'https://api.github.com/search/repositories'
+    payload = {
+        'q': 'created:>={}'.format(date),
+        'sort': 'stars',
+        'per_page': '{}'.format(top_size),
+    }
+    return requests.get(url, params=payload).json()['items']
+
+
+def print_trending_repositories(repos):
+    print('Stars Issues URL')
+    for item in top_repos:
+        print('{0:>5} {1:^6} {2}'.format(
+            item['stargazers_count'],
+            item['open_issues_count'],
+            item['html_url']
+            )
+        )
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    period = 7
+    try:
+        top_repos = get_trending_repositories(top_size, get_date(period))
+    except requests.exceptions.ConnectionError:
+        print('Failed to establish connection to api.github.com')
+    else:
+        print_trending_repositories(top_repos)
